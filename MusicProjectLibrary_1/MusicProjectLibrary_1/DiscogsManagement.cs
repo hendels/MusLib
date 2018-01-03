b9378bb0b564e52cedaa7923881e4acfe719a93e
hendels <p.harendarz@gmail.com>
2018-01-03 21:19:00 +0100

    partially working IObservable Subscription Reading

--- MusicProjectLibrary_1/MusicProjectLibrary_1/DiscogsManagement.cs
+++ MusicProjectLibrary_1/MusicProjectLibrary_1/DiscogsManagement.cs
@@ -8,12 +8,16 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using DiscogsClient;
 using RestSharpHelper.OAuth1;
-
+using System.Reactive;
 
 namespace MusicProjectLibrary_1
 {
     class DiscogsManagement
     {
+        public void go()
+        {
+
+        }
         public class SearchResultsDiscogs
         {
             public string title { get; set; }
@@ -24,30 +28,55 @@ namespace MusicProjectLibrary_1
         {
             var tokenInformation = new TokenAuthenticationInformation("QWgIjbVsUoULLGCvqwpQZUvsTEFZlmRALsOJJrLv");
             var discogsClient = new DiscogsClient.DiscogsClient(tokenInformation);
-            
+
             var discogsSearch = new DiscogsSearch()
             {
                 artist = "wisp",
-                //release_title = "The Shape Of Jazz To Come"
+                release_title = "Building Dragons"
             };
-            
+
             //Retrieve observable result from search
             var observable = discogsClient.Search(discogsSearch);
-            IObserver<int> obsvr = Observer.Create<int>();
-            List <SearchResultsDiscogs> LSRD = new List<SearchResultsDiscogs>();
+            //IObserver<DiscogsSearch> obsvr = Observer.Create<DiscogsSearch>(x => listBoxConsole.Add(x.type));
+            //obsvr.OnCompleted
+            List<SearchResultsDiscogs> LSRD = new List<SearchResultsDiscogs>();
             //SearchResultsDiscogs SRD = new SearchResultsDiscogs();
-
-            //IDisposable sub1 = observable.Subscribe(x => SRD.title = x.title);
-            using (observable.Subscribe(x => listBoxConsole.Add($"{x.title}{x.style}{x.type}")))
+            //observable.Subscribe(SearchResultsDiscogs, ex => { throw ex; });
+            //using (observable.Subscribe(x => listBoxConsole.Add(x.title)))
+            //{
+                //listBoxConsole.Add(LSRD.Count.ToString());
+            //}
+            
+            IDisposable sub1 = observable.Subscribe(x =>                       
             {
-                
                 SearchResultsDiscogs SRD = new SearchResultsDiscogs();
-                SRD.type = x.type;
+                SRD.title = x.title;
+                SRD.type = x.type.ToString();
+                SRD.year = Convert.ToInt32(x.year);
+                LSRD.Add(SRD);
+                //listBoxConsole.Add(LSRD.Count.ToString());
+                //throw LSRD();
+            }
+            );
+            
+            //sub1.
+            //sub1.Dispose();
+
+            MessageBox.Show("click ok...");
+            foreach (SearchResultsDiscogs itemSRD in LSRD)
+            {
+                if (itemSRD.type == "master")
+                {
+                    listBoxConsole.Add(itemSRD.title + "/" + itemSRD.year);
+                }
+                
             }
 
 
-            IDisposable sub2 = observable.Subscribe(x => listBoxConsole.Add(x.title));
-            
+            //IDisposable sub2 = observable.Subscribe(x => listBoxConsole.Add(x.title));
+
         }
+        
     }
+
 }
